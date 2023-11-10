08ce12b4c2f9793efdd70f1329ff6d0acfd51ed7
wenjieli <2773015163@qq.com>
2023-11-10 19:01:08 +0800

    first

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     proxy: {
-        "/moon-server": {
-          target: "http://43.134.115.222:8080/moon-server/",
-          changeOrigin: true,
-          ws: true,
-          pathRewrite: { "^/moon-server": "" },
-        },
+      '/moon-server': {
+        target: 'http://43.134.115.222:8080/moon-server/',
+        changeOrigin: true,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/moon-server/, ''),
       },
-  }
+    },
+  },
 });
